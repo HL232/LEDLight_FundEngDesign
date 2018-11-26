8fdeec60d2c95b4e80e344d4f68c3fe9b77c3416
Howard <howard.li@duke.edu>
2018-11-26 00:34:26 -0500

    Adding music to Arduino Code

--- Software/ArduinoCode_LEDLight/ArduinoCode_LEDLight.ino
+++ Software/ArduinoCode_LEDLight/ArduinoCode_LEDLight.ino
@@ -1,3 +1,6 @@
+//**********************************************************************************************************************************
+//******************************************************************Stuff for LED***************************************************
+//**********************************************************************************************************************************
 #define button_pin 8 //The push button is on pin 8
 #define led_pin 9 //The LED is on PWM pin 9
 #define blinkTime 250 //Blink time
@@ -12,20 +15,6 @@ int modSwitchState;
 unsigned long previousTime = 0;       
 int ledBlinkState = LOW;
 
-void setup() {
-  //Initialize pins. led pin is an output, button pin is an input
-  pinMode(led_pin, OUTPUT);
-  pinMode(button_pin, INPUT);
-  pinMode(annoyingLight, OUTPUT);
-  //I'm turning off the annoying LED built into pin 13
-  digitalWrite(annoyingLight, LOW);
-  //Instead of using attachInterrupt(), I coded my own interrupt function
-}
-
-void loop() {
-  button_push();
-  set_LED_state();
-}
 
 void button_push(){
     //Keep track of the previous state of the button. Records when the button is released. Add 1 to the counter for every button push
@@ -83,4 +72,180 @@ void flashing() {
       }
       digitalWrite(led_pin, ledBlinkState);
     }
+}
+//**********************************************************************************************************************************
+//******************************************************************Stuff for Music*************************************************
+//**********************************************************************************************************************************
+// Music code based on : https://www.arduino.cc/en/Tutorial/PlayMelody
+// TONES  ==========================================
+// Start by defining the relationship between 
+//       note, period, &  frequency. 
+#define  D4     3400    // 294 Hz 
+#define  E     3038    // 329 Hz 
+#define  F     2864    // 349 Hz 
+#define  G     2550    // 392 Hz 
+#define  A     2272    // 440 Hz 
+#define  B     2028    // 493 Hz 
+#define  C     1912    // 523 Hz 
+#define D5     1703
+
+// DURATION OF THE NOTES 
+#define BPM 200    //  you can change this value changing all the others
+#define h 2*q //half 2/4
+#define q 6000/BPM //quarter 1/4 
+#define e q/2   //eighth 1/8
+#define w 4*q // whole 4/4
+#define d 3*q //dotted half
+
+
+// SETUP ============================================
+// Set up speaker on a PWM pin (digital 9, 10 or 11)
+#define speakerOut 11 //The speaker output is on pin 11
+#define button_pin_music 12 //The push button is on pin 12
+
+// MELODY and TIMING  =======================================
+//  melody[] is an array of notes, accompanied by beats[], 
+//  which sets each note's relative length (higher #, longer note) 
+int melody[] = {   
+  G, A, G, E, C,
+  A, G,
+  G, A, G, A, G, C,
+  B,
+  F, G, F, D4, B, 
+  A, G,
+  G, A, G, A, G, A,
+  E,
+  //Measure 9 
+  G, A, G, E, C,
+  A, G,
+  G, A, G, A, G, C,
+  B,
+  F, G, F, D4, B, 
+  A, G,
+  G, A, G, A, G, A,
+  E,
+  //Measure 17
+  A, A, C, A,
+  G, E, G,
+  F, A, G, F,
+  E,
+  D4, E, G, A,
+  B, B, B,
+  D5, C, B, A,
+  G, F, D4,
+  //Measure 25
+  G, A, G, E, C,
+  A, G,
+  G, A, G, A, G, C,
+  B,
+  F, G, F, D4, B, 
+  A, G,
+  G, A, G, A, G, D5,
+  C};
+int beats[]  = {   e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, 
+  e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, 
+  //Measure 9
+  e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, 
+  e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, 
+  //Measure 17
+  q, q, q, q, 
+  q, q, h, 
+  q, q, q, q, 
+  w, 
+  q, q, q, q, 
+  q, q, h, 
+  q, q, q, q, 
+  q, q, h, 
+  //Measure 25
+  e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, 
+  e, q, e, q, q,
+  q, d,
+  e, e, e, e, q, q, 
+  w, }; 
+int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping.
+
+// Set overall tempo
+long tempo = 10000;
+// Set length of pause between notes
+int pause = 500;
+// Loop variable to increase Rest length
+int rest_count = 100;
+
+// Initialize core variables
+int tone_ = 0;
+int beat = 0;
+long duration  = 0;
+
+//These two variables keep track of the button pushes
+int pushState_music = 0;           
+int prevPushState_music;
+
+// PLAY TONE  ==============================================
+// Pulse the speaker to play a tone for a particular duration
+void playTone() {
+  long elapsed_time = 0;
+  if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
+    //  played less long than 'duration', pulse speaker HIGH and LOW
+    while (elapsed_time < duration) {
+
+      digitalWrite(speakerOut,HIGH);
+      delayMicroseconds(tone_ / 2);
+
+      // DOWN
+      digitalWrite(speakerOut, LOW);
+      delayMicroseconds(tone_ / 2);
+
+      // Keep track of how long we pulsed
+      elapsed_time += (tone_);
+    } 
+  }                           
+}
+
+void play_music(){
+  prevPushState_music = pushState_music;
+  pushState_music = digitalRead(button_pin_music);
+  if (prevPushState_music > pushState_music) {
+    for (int i=0; i<MAX_COUNT; i++) {
+      tone_ = melody[i];
+      beat = beats[i];
+      duration = beat * tempo; // Set up timing
+      playTone(); 
+      // A pause between notes...
+      delayMicroseconds(pause);
+    }
+  }
+}
+//**********************************************************************************************************************************
+//******************************************************************SETUP AND LOOP**************************************************
+//**********************************************************************************************************************************
+void setup() {
+  //Initialize pins. led pin is an output, button pin is an input
+  pinMode(led_pin, OUTPUT);
+  pinMode(button_pin, INPUT);
+  pinMode(annoyingLight, OUTPUT);
+  //I'm turning off the annoying LED built into pin 13
+  digitalWrite(annoyingLight, LOW);
+  pinMode(button_pin_music, INPUT);
+  pinMode(speakerOut, OUTPUT);
+}
+
+void loop() {
+  button_push();
+  set_LED_state();
+  play_music();
 }
